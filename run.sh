28a95f7e69edcb5c1e25aa72480d9bc13b8a5f28
Amir Samary <amirsamary@gmail.com>
2019-12-13 18:40:30 -0500

    Fixing auto build

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#docker run --name synthea-docker phillip/synthea-docker:latest
-
 docker run --rm -v $PWD/output:/output --name synthea-docker intersystemsdc/irisdemo-base-synthea:latest -p 5
