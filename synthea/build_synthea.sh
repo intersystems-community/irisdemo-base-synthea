6a376771bf4420d084e9e778f22d82c0ff9768ef
Amir Samary <amirsamary@gmail.com>
2019-12-13 19:12:45 -0500

    Fixing auto build

--- synthea/build_synthea.sh
+++ synthea/build_synthea.sh
@@ -6,7 +6,7 @@ rm -rf ./synthea
 
 git clone --depth 1 https://github.com/synthetichealth/synthea.git
 
-docker run --rm -u gradle -v "$PWD/synthea":/home/gradle/project -w /home/gradle/project gradle gradle assemble
+docker run --rm -u gradle -v ./synthea:/home/gradle/project -w /home/gradle/project gradle gradle assemble
 
 rm -rf ./dist
 mkdir ./dist
